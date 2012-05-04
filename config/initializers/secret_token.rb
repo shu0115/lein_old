340142c6b3c729bff49ce0ace9cad9623b4b1b76
shu_0115 <raisondetre0115@gmail.com>
2012-05-04 17:43:45 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lein::Application.config.secret_token = 'b74176eec37cf44af8334d9627703d319f3cfe727f9f41c5d39f493e492e69386b433682b5c1077ddef3e224c3b2476e6f437626e8cc8628d4f93e2c7628ffa0'
